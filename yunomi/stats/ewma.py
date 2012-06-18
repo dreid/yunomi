4a5292647c6a0ffe01eed299deaafe25e1b40a70
David Reid <dreid@dreid.org>
2012-06-17 21:29:23 -0700

    EWMA

---
+++ yunomi/stats/ewma.py
@@ -0,0 +1,47 @@
+from math import exp
+
+
+class EWMA(object):
+    INTERVAL = 5
+    SECONDS_PER_MINUTE = 60.0
+    ONE_MINUTE = 1
+    FIVE_MINUTES = 5
+    FIFTEEN_MINUTES = 15
+    M1_ALPHA = 1 - exp(-INTERVAL / SECONDS_PER_MINUTE / ONE_MINUTE)
+    M5_ALPHA = 1 - exp(-INTERVAL / SECONDS_PER_MINUTE / FIVE_MINUTES)
+    M15_ALPHA = 1 - exp(-INTERVAL / SECONDS_PER_MINUTE / FIFTEEN_MINUTES)
+
+    def __init__(self, alpha, interval=None):
+        self._alpha = alpha
+        self._interval = (interval or EWMA.INTERVAL) * 1000000000
+        self._uncounted = 0.0
+        self._rate = None
+
+    @classmethod
+    def oneMinuteEWMA(klass):
+        return klass(klass.M1_ALPHA)
+
+    @classmethod
+    def fiveMinuteEWMA(klass):
+        return klass(klass.M5_ALPHA)
+
+    @classmethod
+    def fifteenMinuteEWMA(klass):
+        return klass(klass.M15_ALPHA)
+
+    def update(self, value):
+        self._uncounted += value
+
+    def tick(self):
+        count = self._uncounted
+        self._uncounted = 0.0
+
+        instantRate = count / self._interval
+
+        if self._rate:
+            self._rate += (self._alpha * (instantRate - self._rate))
+        else:
+            self._rate = instantRate
+
+    def getRate(self):
+        return self._rate * 1000000000
---
+++ yunomi/tests/test_EWMA.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+
+from yunomi.stats.ewma import EWMA
+
+
+class EWMATests(TestCase):
+    def elapseMinute(self, ewma):
+        for i in xrange(0, 12):
+            ewma.tick()
+
+    def test_oneMinuteEWMA(self):
+        ewma = EWMA.oneMinuteEWMA()
+
+        ewma.update(3)
+        ewma.tick()
+
+        for expectedRate in [0.6, 0.22072766, 0.08120117, 0.02987224,
+                             0.01098938, 0.00404277, 0.00148725,
+                             0.00054713, 0.00020128, 0.00007405]:
+            self.assertAlmostEqual(ewma.getRate(), expectedRate)
+            self.elapseMinute(ewma)
+
+    def test_fiveMinuteEWMA(self):
+        ewma = EWMA.fiveMinuteEWMA()
+
+        ewma.update(3)
+        ewma.tick()
+
+        for expectedRate in [0.6, 0.49123845, 0.40219203, 0.32928698, 0.26959738,
+                             0.22072766, 0.18071653, 0.14795818, 0.12113791,
+                             0.09917933]:
+            self.assertAlmostEqual(ewma.getRate(), expectedRate)
+            self.elapseMinute(ewma)
+
+    def test_fifteenMinuteEWMA(self):
+        ewma = EWMA.fifteenMinuteEWMA()
+
+        ewma.update(3)
+        ewma.tick()
+
+        for expectedRate in [0.6, 0.56130419, 0.52510399, 0.49123845, 0.45955700,
+                             0.42991879, 0.40219203, 0.37625345, 0.35198773,
+                             0.32928698]:
+            self.assertAlmostEqual(ewma.getRate(), expectedRate)
+            self.elapseMinute(ewma)

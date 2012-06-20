a936a970c0b20c5173e51446a40fcca8b5867149
Richie Zeng <richzeng@gmail.com>
2012-06-20 10:27:08 -0700

    Added time dependent test for exp_decay_sample

--- yunomi/stats/exp_decay_sample.py
+++ yunomi/stats/exp_decay_sample.py
@@ -11,21 +11,24 @@ class ExponentiallyDecayingSample(object):
     values = {}
     next_scale_time = 0
 
-    def __init__(self, reservoir_size, alpha):
+    def __init__(self, reservoir_size, alpha, clock=time):
         self.reservoir_size = reservoir_size
         self.alpha = alpha
+        self.clock = clock
         self.clear()
 
     def clear(self):
-       self.count = 0
-       self.values = {}
-       self.start_time = time()
-       self.next_scale_time = time() + self.RESCALE_THRESHOLD
+        self.count = 0
+        self.values = {}
+        self.start_time = self.clock()
+        self.next_scale_time = self.clock() + self.RESCALE_THRESHOLD
 
     def size(self):
         return min(self.reservoir_size, self.count)
 
-    def update(self, value, timestamp = time()):
+    def update(self, value, timestamp=None):
+        if not timestamp:
+            timestamp = self.clock()
         self._rescale_if_needed()
         priority = self._weight(timestamp - self.start_time) / random()
         self.count += 1
@@ -42,7 +45,7 @@ class ExponentiallyDecayingSample(object):
                     del self.values[first]
 
     def _rescale_if_needed(self):
-        now = time()
+        now = self.clock()
         next_ = self.next_scale_time
         if now >= next_:
             self._rescale(now, next_)
@@ -57,13 +60,13 @@ class ExponentiallyDecayingSample(object):
         if self.next_scale_time == next_:
             self.next_scale_time = now + self.RESCALE_THRESHOLD
             old_start_time = self.start_time
-            self.start_time = time()
+            self.start_time = self.clock()
             keys = self.values.keys()
             keys.sort()
 
             for key in keys:
                 value = self.values[key]
                 del self.values[key]
-                self.values[key * exp(-alpha * (start_time - old_start_time))] = value
+                self.values[key * exp(-self.alpha * (self.start_time - old_start_time))] = value
 
             self.count = len(self.values)
--- yunomi/tests/exp_decay_sample_test.py
+++ yunomi/tests/exp_decay_sample_test.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from twisted.internet.task import Clock
+
 from yunomi.stats.exp_decay_sample import ExponentiallyDecayingSample
 from yunomi.stats.snapshot import Snapshot
 
@@ -41,3 +43,29 @@ class ExponentialDecaySapmleTests(TestCase):
 
         for i in snapshot.get_values():
             self.assertTrue(i < 100 and i >= 0)
+
+    def test_long_period_of_inactivity_should_not_corrupt_sampling_state(self):
+        twisted_clock = Clock()
+        sample = ExponentiallyDecayingSample(10, 0.015, twisted_clock.seconds)
+        for i in xrange(1000):
+            sample.update(1000 + i)
+            twisted_clock.advance(0.1)
+
+        self.assertTrue(sample.get_snapshot().size() == 10)
+        self._assert_all_values_between(sample, 1000, 2000)
+
+        twisted_clock.advance(15*3600)
+        sample.update(2000)
+        self.assertTrue(sample.get_snapshot().size() == 2)
+        self._assert_all_values_between(sample, 1000, 3000)
+
+        for i in xrange(1000):
+            sample.update(3000 + i)
+            twisted_clock.advance(0.1)
+
+        self.assertTrue(sample.get_snapshot().size() == 10)
+        self._assert_all_values_between(sample, 3000, 4000)
+
+    def _assert_all_values_between(self, sample, lower, upper):
+        for value in sample.get_snapshot().get_values():
+            self.assertTrue(value >= lower and value < upper)

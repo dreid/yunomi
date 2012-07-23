7ec1f7b9b0ae8ffe135b95c62a03598e6fe8efc9
David <dreid@dreid.org>
2012-07-23 11:36:23 -0700

    Add class methods for getting biased and uniform histograms so that we can have a unique Sample instances per histogram.

--- yunomi/tests/test_histogram.py
+++ yunomi/tests/test_histogram.py
@@ -6,8 +6,16 @@ from yunomi.core.histogram import Histogram
 class HistogramTests(TestCase):
 
     def setUp(self):
-        self.histogram_b = Histogram(Histogram.SampleType.BIASED)
-        self.histogram_u = Histogram(Histogram.SampleType.UNIFORM)
+        self.histogram_b = Histogram.get_biased()
+        self.histogram_u = Histogram.get_uniform()
+
+    def test_unique_biased_histogram(self):
+        new_histogram = Histogram.get_biased()
+        self.assertIsNot(new_histogram.sample, self.histogram_b.sample)
+
+    def test_unique_uniform_histogram(self):
+        new_histogram = Histogram.get_uniform()
+        self.assertIsNot(new_histogram.sample, self.histogram_u.sample)
 
     def test_empty_histogram(self):
         for histogram in self.histogram_b, self.histogram_u:

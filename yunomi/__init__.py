44dccee8e1a175cedf1cbf057f14e5cf0cdcb23a
Richie Zeng <richzeng@gmail.com>
2012-07-25 14:31:49 -0700

    made event type optional in meter

---
+++ yunomi/__init__.py
@@ -0,0 +1,6 @@
+from yunomi.core.metrics_registry import MetricsRegistry, Counter, Histogram,\
+                                         Meter, Timer, counter, histogram,\
+                                         meter, timer, dump_metrics
+
+__all__ = ['MetricsRegistry', 'Counter', 'Histogram', 'Meter', 'Timer',
+           'counter', 'histogram', 'meter', 'timer', 'dump_metrics']

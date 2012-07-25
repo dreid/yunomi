5e07d69a5d73117ff099a150335d7ed2dc84bf49
Richie Zeng <richzeng@gmail.com>
2012-07-25 11:57:54 -0700

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="yunomi",
+    version="0.1.0",
+    description="metrics library",
+    packages=["yunomi"],
+    install_requires=["Twisted", "unittest2"]
+)

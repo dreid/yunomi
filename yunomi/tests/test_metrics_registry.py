4e937ee165fcb3c12e14bd46bceb9c799cd935d3
Richie Zeng <richzeng@gmail.com>
2012-07-25 11:18:30 -0700

    better test coverage for metrics registry

--- yunomi/tests/test_metrics_registry.py
+++ yunomi/tests/test_metrics_registry.py
@@ -1,7 +1,5 @@
 from unittest2 import TestCase
 
-from twisted.internet.task import Clock
-
 from yunomi.core.metrics_registry import MetricsRegistry
 
 
@@ -22,15 +20,16 @@ class MetricsRegistryTests(TestCase):
         dump = self.registry.dump_metrics()
 
         self.assertEqual(len(dump), 25)
-
-        for metric in ("counter_count", "histogram_avg", "histogram_max",
-                       "histogram_min", "histogram_std_dev",
-                       "histogram_75_percentile", "histogram_98_percentile",
-                       "histogram_99_percentile", "histogram_999_percentile",
-                       "meter_15m_rate", "meter_5m_rate", "meter_1m_rate",
-                       "meter_mean_rate", "timer_avg", "timer_max",
-                       "timer_min", "timer_std_dev", "timer_75_percentile",
-                       "timer_98_percentile", "timer_99_percentile",
-                       "timer_999_percentile", "timer_15m_rate", "timer_5m_rate",
-                       "timer_1m_rate", "timer_mean_rate"):
-            self.assertTrue(metric in [stat["name"] for stat in dump])
+        metric_names = ("counter_count", "histogram_avg", "histogram_max",
+                        "histogram_min", "histogram_std_dev",
+                        "histogram_75_percentile", "histogram_98_percentile",
+                        "histogram_99_percentile", "histogram_999_percentile",
+                        "meter_15m_rate", "meter_5m_rate", "meter_1m_rate",
+                        "meter_mean_rate", "timer_avg", "timer_max",
+                        "timer_min", "timer_std_dev", "timer_75_percentile",
+                        "timer_98_percentile", "timer_99_percentile",
+                        "timer_999_percentile", "timer_15m_rate",
+                        "timer_5m_rate", "timer_1m_rate", "timer_mean_rate")
+        for stat in dump:
+            self.assertTrue(stat["name"] in metric_names)
+            self.assertEqual(stat["value"], 0)

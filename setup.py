0ffdd144f1c9b5b7f0ec94e9e764ab3637204d90
Richie Zeng <richzeng@gmail.com>
2012-07-26 09:53:25 -0700

    added more information to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup
 setup(
     name="yunomi",
     version="0.1.1",
-    description="metrics library",
+    description="A Python metrics library with rate, statistical distribution, and timing information.",
+    author="richzeng",
+    author_email="richie.zeng@rackspace.com",
+    url="https://github.com/richzeng/yunomi",
     packages=["yunomi"],
     install_requires=["Twisted", "unittest2"]
 )

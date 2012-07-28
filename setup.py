22388ec9f35895d26552285c52991465eddc2fce
Richie Zeng <richzeng@gmail.com>
2012-07-27 17:31:46 -0700

    0.2.0 -> 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="yunomi",
-    version="0.2.0",
+    version="0.2.1",
     description="A Python metrics library with rate, statistical distribution, and timing information.",
     author="richzeng",
     author_email="richie.zeng@rackspace.com",

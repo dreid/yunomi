79d1fdc7b79d4e88958a2f480103dfdfb6b06533
David Reid <dreid@dreid.org>
2013-06-03 05:09:37 -0700

    License

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="yunomi",
-    version="0.2.2",
+    version="0.3.0",
     description="A Python metrics library with rate, statistical distribution, and timing information.",
-    author="richzeng",
-    author_email="richie.zeng@rackspace.com",
-    url="https://github.com/richzeng/yunomi",
-    packages=["yunomi", "yunomi.core", "yunomi.stats"],
+    maintainer="David Reid",
+    maintainer_email="dreid@dreid.org",
+    url="https://github.com/dreid/yunomi",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+
+    ]
+    packages=find_packages()
 )
